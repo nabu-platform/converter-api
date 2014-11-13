cd270eab3bccc5002e42e8f603f4fb9e071d12cb
alex <alex@nabu.be>
2014-11-13 15:58:55 +0100

    Added another layer of service loading

--- src/main/java/be/nabu/libs/converter/ConverterFactory.java
+++ src/main/java/be/nabu/libs/converter/ConverterFactory.java
@@ -1,5 +1,7 @@
 package be.nabu.libs.converter;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ServiceLoader;
@@ -27,12 +29,40 @@ public class ConverterFactory {
 		this.converters.remove(converter);
 	}
 	
+	@SuppressWarnings("unchecked")
 	public Converter getConverter() {
 		if (converter == null) {
 			if (converters.isEmpty()) {
-				ServiceLoader<Converter> serviceLoader = ServiceLoader.load(Converter.class);
-				for (Converter converter : serviceLoader)
-					converters.add(converter);
+				try {
+					// let's try this with custom service loading based on a configuration
+					Class<?> clazz = Thread.currentThread().getContextClassLoader().loadClass("be.nabu.utils.services.ServiceLoader");
+					Method declaredMethod = clazz.getDeclaredMethod("load", Class.class);
+					converters.addAll((List<Converter>) declaredMethod.invoke(null, Converter.class));
+				}
+				catch (ClassNotFoundException e) {
+					// ignore, the framework is not present
+				}
+				catch (NoSuchMethodException e) {
+					// corrupt framework?
+					throw new RuntimeException(e);
+				}
+				catch (SecurityException e) {
+					throw new RuntimeException(e);
+				}
+				catch (IllegalAccessException e) {
+					// ignore
+				}
+				catch (InvocationTargetException e) {
+					// ignore
+				}
+				// if there are still no instances, fall back to SPI
+				// it is actually possible that you _are_ using the custom service loader, but 
+				if (converters.isEmpty()) {
+					ServiceLoader<Converter> serviceLoader = ServiceLoader.load(Converter.class);
+					for (Converter converter : serviceLoader) {
+						converters.add(converter);
+					}
+				}
 			}
 			converter = new MultipleConverter(converters);
 		}
