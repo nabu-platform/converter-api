0b988f8aa7420526918f956ca32bd4aaa80396af
alex <alex@nabu.be>
2014-11-17 14:18:58 +0100

    Localize class loading

--- src/main/java/be/nabu/libs/converter/ConverterFactory.java
+++ src/main/java/be/nabu/libs/converter/ConverterFactory.java
@@ -35,7 +35,7 @@ public class ConverterFactory {
 			if (converters.isEmpty()) {
 				try {
 					// let's try this with custom service loading based on a configuration
-					Class<?> clazz = Thread.currentThread().getContextClassLoader().loadClass("be.nabu.utils.services.ServiceLoader");
+					Class<?> clazz = getClass().getClassLoader().loadClass("be.nabu.utils.services.ServiceLoader");
 					Method declaredMethod = clazz.getDeclaredMethod("load", Class.class);
 					converters.addAll((List<Converter>) declaredMethod.invoke(null, Converter.class));
 				}
